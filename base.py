525681715babb7f80c66141deb4158271eff8ffe
Huib van Nieuwenhuijze <Huib.van.Nieuwenhuijze@gmail.com>
2025-11-17 20:54:12 +0100

    Create base.py

---
+++ base.py
@@ -0,0 +1,30 @@
+#!python3
+import os
+import datetime
+
+# Test input
+input = [""]
+
+# input integers separated by commas
+#input= [int(i) for i in open("input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().split(",")]
+
+# input integers on complete lines
+#input = [int(i) for i in open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()]
+
+# input complete lines
+#input = open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()
+
+
+def solve1():
+    print("Deel 1: No")
+
+def solve2():
+    print("Deel 2: No")
+
+start = datetime.datetime.now()
+solve1()
+print("Tijd voor oplossing 1: ", datetime.datetime.now()-start)
+
+start = datetime.datetime.now()
+solve2()
+print("Tijd voor oplossing 2: ", datetime.datetime.now()-start)
