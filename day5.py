edddd818a32e7a1149df8ba6ddcef35aa5c4089e
Huib van Nieuwenhuijze <Huib.van.Nieuwenhuijze@gmail.com>
2025-12-22 20:06:33 +0100

    Day 5: working on test input and not on real input (again...). Result is too low

--- day5.py
+++ day5.py
@@ -16,7 +16,7 @@ input = ["3-5",
 "32"]
 
 # input complete lines
-#input = open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()
+input = open('input_'+os.path.basename(__file__).split(".")[0]+'.txt').read().splitlines()
 
 def solve1():
     result = 0
@@ -37,7 +37,7 @@ def solve1():
     print("Deel 1: " + str(result))
 
 def solve2():
-    result = -1
+    result = 0
     fresh_stuff = {}
 
     for line in input:
@@ -46,18 +46,26 @@ def solve2():
         fresh_stuff[line.split('-')[0]] = (int(line.split('-')[0]),int(line.split('-')[1])+1)
     
     startjes = sorted([int(f) for f in fresh_stuff.keys()])
-    print(startjes)
+#    print(startjes)
+    currentstart = startjes[0]
+    currentend = fresh_stuff[str(startjes[0])][1]
     for i in range(len(startjes)):
-        print(startjes[i])
-        # if i is not the last item: 
-        #   if the next start is larger than the current end: count all numbers in the range
-        #   else if the next stop is larger than the current stop: count all number from the current start to the next start
-        #   else ignore the smaller next range
-
-        # Take the start
+#        print(startjes[i])
+        # Take the first range
+        # if the next start is after the range: count the current range and restart with the next range
         # until the next start is smaller than the current end
         # if the next end is smaller than the current end: ignore range
-        # if the next end is larger than the current end: start working with that end
+        # if the next end is larger than the current end: update the end and continue
+        if (i == len(startjes)-1):
+            result += (currentend - currentstart) + 1
+            continue
+        if (startjes[i+1] > currentend):
+            result += (currentend - currentstart) + 1
+            currentstart = startjes[i+1]
+            currentend = fresh_stuff[str(startjes[i+1])][1]
+        else:
+            if (fresh_stuff[str(startjes[i+1])][1] > currentend):
+                currentend = fresh_stuff[str(startjes[i+1])][1]
 
 
  
